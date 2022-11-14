fb6d7847c996eb3b8131173cfb633f2ab13e136e
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-14 16:22:11 -0300

    ⚡️ lazy image

--- src/app/core/services/navigation.service.ts
+++ src/app/core/services/navigation.service.ts
@@ -7,37 +7,46 @@ import { Observable, of } from 'rxjs';
 })
 export class NavigationService {
   items: MenuItem[] = [
-    {
-      label: 'Cinemar',
-      icon: 'bi bi-film',
-      routerLink: '/private'
-    },
+    // {
+    //   label: 'Cinemar',
+    //   icon: 'bi bi-film',
+    //   routerLink: '/private'
+    // },
     {
       label: 'Peliculas',
       icon: 'bi bi-camera-reels-fill',
-      items: [
-        {
-          label: 'Estrenos',
-          icon: 'pi pi-megaphone',
-        },
-        {
-          label: 'Todas las peliculas',
-          icon: 'bi bi-camera-reels',
-        },
-        {
-          label: 'Peliculas 2-D',
-          icon: 'bi bi-camera-video-fill',
-        },
-        {
-          label: 'Peliculas 3-D',
-          icon: 'bi bi-badge-3d-fill'
-        },
-      ],
+      routerLink: '/home'
+      // items: [
+      //   {
+      //     label: 'Estrenos',
+      //     icon: 'pi pi-megaphone',
+      //   },
+      //   {
+      //     label: 'Todas las peliculas',
+      //     icon: 'bi bi-camera-reels',
+      //   },
+      //   {
+      //     label: 'Peliculas 2-D',
+      //     icon: 'bi bi-camera-video-fill',
+      //   },
+      //   {
+      //     label: 'Peliculas 3-D',
+      //     icon: 'bi bi-badge-3d-fill'
+      //   },
+      // ],
     },
     {
       label: 'Mis Reservas',
       icon: 'pi pi-fw pi-calendar',
     },
+    {
+      label: 'Salas',
+      icon: 'pi pi-fw pi-calendar',
+    },
+    {
+      label: 'Descuentos',
+      icon:'bi bi-currency-dollar'
+    }
   ];
   constructor() {}
 
