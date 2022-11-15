1b3bb9aa3ac60c82dbde0a5a274daf54cfa6ebe0
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-15 16:37:42 -0300

    🐛 connection with api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// apiUrl:  'http://127.0.0.1:5000/',
 
 export const environment = {
   production: false,
-  // apiUrl:  'http://127.0.0.1:5000/',
   apiUrl:  'https://cinemar-api.vercel.app/',
 };
 
